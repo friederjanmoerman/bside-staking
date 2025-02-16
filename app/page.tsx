ed2db8c5d653256dede987663938898dc6022b2d
Hardcodepunk <howl@hardcodepunk.be>
2025-02-16 21:21:00 +0530

    walletconnect with bera integration

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1>B Side Application</h1>
+      <div className="mb-8">
+        <w3m-button />
+      </div>
+    </main>
+  )
+}
